12f3d24af15ff39c02183ad5e7e066af524df430
Kevin Nuñez <seidnez@gmail.com>
2024-05-06 23:12:15 -0500

    Agregando clase principal para ejecucion

--- src/kevcold/alura/challengeconversor/principal/Principal.java
+++ src/kevcold/alura/challengeconversor/principal/Principal.java
@@ -1,11 +1,10 @@
 package kevcold.alura.challengeconversor.principal;
 
-import kevcold.alura.challengeconversor.modelos.Consumiendo_Exchange;
+import kevcold.alura.challengeconversor.modelos.SeleccionMonedas;
 
-public class Prueba {
+public class Principal {
     public static void main(String[] args) {
-        Consumiendo_Exchange cop = new Consumiendo_Exchange();
-
-        System.out.println(cop.BuscarMonedas("USD"));
+        SeleccionMonedas seleccionar = new SeleccionMonedas();
+        seleccionar.seleccionar();
     }
 }
